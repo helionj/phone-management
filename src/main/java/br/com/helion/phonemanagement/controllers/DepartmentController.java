59ecc9630afdd78691d8a12dac18551cf64de14d
helionporto <helionporto@hotmail.com>
2021-09-12 11:31:40 -0400

    CRUD Department

--- src/main/java/br/com/helion/phonemanagement/controllers/DepartmentController.java
+++ src/main/java/br/com/helion/phonemanagement/controllers/DepartmentController.java
@@ -1,12 +1,19 @@
 package br.com.helion.phonemanagement.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.helion.phonemanagement.dtos.DepartmentDTO;
 import br.com.helion.phonemanagement.services.DepartmentService;
@@ -23,6 +30,26 @@ public class DepartmentController {
 		
 		List<DepartmentDTO> list = service.findAll();
 		return  ResponseEntity.ok().body(list);
+	}
+	@GetMapping(value="/{id}")
+	public ResponseEntity<DepartmentDTO> findById(@PathVariable Long id){
+		
+		DepartmentDTO dto = service.findById(id);
+		return  ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<DepartmentDTO> insert(@RequestBody DepartmentDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<DepartmentDTO> update(@PathVariable Long id, @RequestBody DepartmentDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
--- src/main/java/br/com/helion/phonemanagement/services/DepartmentService.java
+++ src/main/java/br/com/helion/phonemanagement/services/DepartmentService.java
@@ -1,8 +1,10 @@
 package br.com.helion.phonemanagement.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import br.com.helion.phonemanagement.dtos.DepartmentDTO;
 import br.com.helion.phonemanagement.entities.Department;
 import br.com.helion.phonemanagement.repositories.DepartmentRepository;
+import br.com.helion.phonemanagement.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class DepartmentService {
@@ -22,5 +25,34 @@ public class DepartmentService {
 	public List<DepartmentDTO> findAll(){
 		List<Department> list  = repository.findAll();
 		return list.stream().map(department -> (new DepartmentDTO(department))).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public DepartmentDTO findById(Long id) {
+		Optional<Department> obj = repository.findById(id);
+		return new DepartmentDTO(obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found")));
+	}
+	
+	@Transactional
+	public DepartmentDTO insert(DepartmentDTO dto) {
+		Department entity = new Department();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new DepartmentDTO(entity);
+	}
+
+	@Transactional
+	public DepartmentDTO update(Long id, DepartmentDTO dto) {
+		try {
+			Department entity = repository.getById(id);
+			entity.setName(dto.getName());
+			entity= repository.save(entity);
+			return new DepartmentDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found "+ id);
+		}
+		
+	
 	}
 }
